e36a706fb9cc0fe34e5468b4eb70ea2227493903
lidia <lidiabara7@gmail.com>
2019-07-22 23:00:43 -0700

    added that app routes and requred express

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-var express = required("express");
-var path = required("path");
+var express = require("express");
+var path = require("path");
 
-//============== setting up the express app
+//setting up the express app
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 //setting up express to handdle parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//adding the application routes
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 //Starting the server to be ready for requests
 app.listen(PORT, function(){
     console.log("app listening on PORT" + PORT)
 })
+
